3ec6b1fc0969782fcd0ae37611ebcde251683d70
Yadira Tamarit <ystamaritq@gmail.com>
2020-06-20 23:14:08 -0500

    added questions for test

--- Develop/index.js
+++ Develop/index.js
@@ -1,12 +1,60 @@
-const questions = [
+const inquirer = require("inquirer");
 
+const questions = [
+	{
+		type: "input",
+		name: "title",
+		message: "What is your project title?",
+	},
+	{
+		type: "input",
+		name: "description",
+		message: "What is the project description?",
+	},
+	{
+		type: "input",
+		name: "installation",
+		message: "Which steps do you have to follow to install the project?",
+	},
+	{
+		type: "input",
+		name: "usage",
+		message: "What is the usage of the project?",
+	},
+	{
+		type: "input",
+		name: "usage",
+		message: "What is the usage of the project?",
+	},
+	{
+		type: "input",
+		name: "license",
+		message: "What is the project license?",
+	},
+	{
+		type: "input",
+		name: "contribution",
+		message: "What is the project contribution?",
+	},
+	{
+		type: "input",
+		name: "tests",
+		message: "What is the project tests cases?",
+	},
+	{
+		type: "list",
+		name: "questions",
+		choices: [
+			"What is the your github profile picture?",
+			"What is the your email?",
+		],
+	},
 ];
 
-function writeToFile(fileName, data) {
-}
+inquirer.prompt(questions).then((data) => {});
 
-function init() {
+function writeToFile(fileName, data) {}
 
-}
+function init() {}
 
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,69 @@
 function generateMarkdown(data) {
-  return `
+	return `
+  
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
 # ${data.title}
+--
+
+## Table of Contents
+
+- [Project Title](#project-title)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+  - [User GitHub profile picture](#user-gitHub-profile-picture)
+  - [User GitHub email](#user-gitHub-email)
+
+--
+
+## Description
+
+${data.description}
+
+--
+
+## Installation
+
+${data.installation}
+
+--
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+[https://choosealicense.com/](https://choosealicense.com/)
+
+## Contributing
+
+${data.contribution}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+### ${data.questions[0]}
+### ${data.questions[1]}
+
+## User GitHub profile picture
+
+## User GitHub email
+
+## Contributing
+
+
+/* template code ends */
 
 `;
 }
